13b2e195d459488d0fe348fa9b033b4eb15cd7dc
Daky <daky>
2024-10-31 11:50:45 +0100

    run.py based on test.py

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import asyncio, sys, traceback
+from config import load_config
+
+async def main():
+    manager = load_config('VRChat', 'config_sample.yaml')
+
+    router = manager.router
+    print("controllers:", list(router.name_to_controller.keys()))
+
+    await manager.start()
+
+
+def handle_exception(loop, context):
+   if (exc := context.get("exception")):
+       print(''.join(traceback.format_exception(type(exc), exc, exc.__traceback__)), file=sys.stderr)
+   else:
+       message = context.get("message", "Unknown error")
+       print(f"Exception with message: {message}", file=sys.stderr)
+
+loop = asyncio.new_event_loop()
+loop.set_exception_handler(handle_exception)
+
+try:
+   loop.run_until_complete(main())
+finally:
+   loop.close()

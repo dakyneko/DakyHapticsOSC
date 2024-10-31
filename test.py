cc4412983dd5f0c1ae9d4877296a25c4d9a0c595
Daky <daky>
2024-10-31 11:36:51 +0100

    example with sample yaml + test

--- test.py
+++ test.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
 
-from base import *
+import asyncio
+from config import load_config
 
 async def main():
-    vrc = VRChat()
-    router = Router(prefix='/avatar/parameters/haptX-')
-    behavior = ProximityBased()
-
-    manager = Manager(vrc, router, behavior, [
-        Controller('headset',
-                   {0: Actuator('headCheekR', min=0.0275, max=0.0392),
-                    1: Actuator('headCheekL', min=0.0275, max=0.0392),
-                    2: Actuator('headTop', min=0.0325, max=0.0588)},
-                   protocol=DakyProtocol(),
-                   connection=UDP('localhost', 1337),
-                   on_battery=False),
-        ])
-
-    print("controllers:", list(router.name_to_controller.keys()))
-
-    async def go():
-        await asyncio.sleep(1)
-        await manager.on_update(router.prefix + 'headCheekR', 0.5) # TODO: test
-    asyncio.create_task(go())
-
+    manager = load_config('VRChat', 'config_sample.yaml')
     await manager.start()
 
 
